34000281d244b4e9b2aedefe48402426b0056595
Gaganpreet kaur <preetkang4511@gmail.com>
2019-09-20 20:44:31 -0400

    Date

--- src/main/java/com/lambton/StringHandling.java
+++ src/main/java/com/lambton/StringHandling.java
@@ -1,11 +1,20 @@
 package com.lambton;
 
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class StringHandling {
     public static void main(String[] args) {
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = null;
         String s4 = new String("Hello");
+
+
         if (s1 == s2) {
             System.out.println("s1 == s2");
         } else {
@@ -27,18 +36,19 @@ public class StringHandling {
             System.out.println("s1 != s4");
         }
         String collegeName = "Lambton College in Toronto";
-         // find the length
+        // find the length
         int nameLength = collegeName.length();
         System.out.println("length " + nameLength);
         // Find the index of Toronto
-         int index = collegeName.indexOf("Toronto");
-         System.out.println("index "  + index);
-         // to do
+        int index = collegeName.indexOf("Toronto");
+        System.out.println("index " + index);
+
+
+        // to do
 
 
         char c[] = collegeName.toCharArray();
-        for (int i = 0 ; i <c.length; i++)
-        {
+        for (int i = 0; i < c.length; i++) {
             System.out.println(c[i]);
         }
 
@@ -46,15 +56,16 @@ public class StringHandling {
         char ch[] = collegeName.toCharArray();
         char rev[] = new char[ch.length];
         int cnt = 0;
-        for (int i = ch.length - 1 ; i >= 0; i--)
-        {
+        for (int i = ch.length - 1; i >= 0; i--) {
             rev[cnt++] = ch[i];
         }
         {
-            String reverseString =  new String(rev);
+            String reverseString = new String(rev);
             System.out.println("Reverse String = " + reverseString);
         }
-
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
 
     }
 }
